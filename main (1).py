6a4ba31d9891426cbcdedbdbc4595bd1027d2ffd
Sumaia-ai <sumaiasultanova64@gmail.com>
2025-10-15 08:17:45 -0400

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -1,28 +1,18 @@
 import java.util.Scanner;
 
-public class Main
-{
-  public static void main(String[] args) {
-    Scanner scan=new Scanner(System.in);
-        int a =scan.nextInt();
-        int c =scan.nextInt();
-        int d =scan.nextInt();
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
 
-       
-        if (a  % 2 == 0  && d% 2 == 0 && c %2 != 0){
-        System.out.println("TRUE");
+        if ((a % 2 == 0 && d % 2 == 0 && c % 2 != 0) || 
+            (a % 2 == 0 && d % 2 != 0 && c % 2 == 0) || 
+            (a % 2 != 0 && d % 2 == 0 && c % 2 == 0)) {
+            System.out.println("TRUE");
+        } else {
+            System.out.println("False");
         }
-        if (a  % 2 == 0 && d %2 != 0 && c% 2 == 0 ){
-        System.out.println("TRUE ");
-        }
-        if (a  % 2 != 0 && d %2 == 0 && c% 2 == 0 ){
-        System.out.println("TRUE ");
-        }
-       
-        
-          else {
-        System.out.println("False");
-         }
-       
-  }
+    }
 }
